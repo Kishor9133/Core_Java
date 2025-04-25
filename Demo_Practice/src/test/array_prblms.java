d4f18503a481cfbceff894f02b037d712c231445
Kishor <kishor.k3113@gmail.com>
2025-04-25 17:36:47 +0530

    array_prblms.java

---
+++ Demo_Practice/src/test/array_prblms.java
@@ -0,0 +1,22 @@
+
+package test;
+
+public class array_prblms {
+
+	public static void main(String[] args) {
+		int [] arr = {3,4,55,6,781,1};
+		int min = arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(min>arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.println(min);
+	}
+
+}
+
+
+
+
+
