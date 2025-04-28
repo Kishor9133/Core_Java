58e3c4ee13d214aaf42edf8e11ec5aeb40c4a60b
Kishor <kishor.k3113@gmail.com>
2025-04-28 21:53:21 +0530

    array_prblms

---
+++ Demo_Practice/src/array_prblms/reverse_an_Array.java
@@ -0,0 +1,13 @@
+package array_prblms;
+import java.util.Arrays;
+import java.util.Collections;
+public class reverse_an_Array {
+
+	public static void main(String[] args) {
+int [] arr = {4,3,6,8,54,3,0};
+int size = arr.length;
+for(int i=size-1;i>=0;i--) {
+}
+	}
+
+}
