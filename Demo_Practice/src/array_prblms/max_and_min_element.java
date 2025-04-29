eb30b176a3cbd2d4d12a1b16da878c5dd1674b21
Kishor <kishor.k3113@gmail.com>
2025-04-29 17:46:05 +0530

    array_prblms

--- Demo_Practice/src/array_prblms/max_and_min_element.java
+++ Demo_Practice/src/array_prblms/max_and_min_element.java
@@ -3,22 +3,22 @@ package array_prblms;
 public class max_and_min_element {
 
 	public static void main(String[] args) {
-int [] arr1 = {6,4,9,2,6,0};
-int [] arr2 = {5,3,7,8,9,4,7};
-int max =arr1[0];
-int min =arr2[0];
-for(int i=1;i<arr1.length;i++) {
+int []arr1 = {5,4,2,3,1,9,0};
+int []arr2 = {4,3,6,8,9,76,4};
+int max = arr1[0];
+int min = arr2[0];
+for (int i=1;i<arr1.length;i++) {
 	if(max < arr1[i]) {
 		max = arr1[i];
-		System.out.println(arr1[i]);
+		System.out.println("The max element is : " + arr1[i]);
 	}
-
-	}
+} 
 for(int j=1;j<arr2.length;j++) {
-	if(min >arr2[j]) {
-		min =arr2[j];
-		System.out.println(arr2[j]);
+	if(min > arr2[j]) {
+		min = arr2[j];
+		System.out.println("The min element is : " + arr2[j]);
 	}
 }
 	}
+
 }
--- Demo_Practice/src/array_prblms/reverse_an_Array.java
+++ Demo_Practice/src/array_prblms/reverse_an_Array.java
@@ -7,6 +7,7 @@ public class reverse_an_Array {
 int [] arr = {4,3,6,8,54,3,0};
 int size = arr.length;
 for(int i=size-1;i>=0;i--) {
+	System.out.print(arr[i] + " ");
 }
 	}
 
