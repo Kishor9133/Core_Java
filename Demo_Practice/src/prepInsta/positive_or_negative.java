eb173addc2621350c01065a3620d2c9caf2aed3b
Kishor <kishor.k3113@gmail.com>
2025-05-06 16:36:24 +0530

    Prepinsta

---
+++ Demo_Practice/src/prepInsta/positive_or_negative.java
@@ -0,0 +1,15 @@
+
+public class positive_or_negative {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			Scanner sc =new  Scanner(System.in);
+			System.out.println("Enter the number");
+			int num = sc.nextInt();
+			if(num<0) {
+				System.out.println("Given number is Positive");
+			}else {
+				System.out.println("Given number is Negative");
+	}
+
+}
---
+++ Demo_Practice/src/prepInsta/sum_of_natural_numbers.java
@@ -0,0 +1,10 @@
+package prepinsta;
+
+public class sum_of_natural_numbers {
+
+	public static void main(String[] args) {
+	int n=10;
+	System.out.println("The sum of N natural numbers are: " + n*(n+1)/2);
+	}
+
+}
